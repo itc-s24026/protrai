51af248a9f4d7f608a4adcf024f441d64e9ad3da
itc-s24026 <s24026@std.it-college.ac.jp>
2024-06-10 15:29:56 +0900

    if_kadai.py.txt

---
+++ if_kadai.py
@@ -0,0 +1,12 @@
+#s24026
+#if文条件式
+
+score >= 50
+if score >= 60:
+    print("可")
+elif score >= 70:
+    print("良")
+elif score >= 80:
+    print("优")
+else:
+    print("不可")
---
+++ 条件式.py
@@ -0,0 +1,6 @@
+score = 90
+if score >=80:
+    print("OK")
+    print("YEAR")
+else:
+    print("haha")
