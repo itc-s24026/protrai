92c0b3fb0b5eb4323df4724b867bddf8ea97b339
itc-s24026 <s24026@std.it-college.ac.jp>
2024-06-11 09:40:03 +0900

    for_kaidai.py

---
+++ for_kaidai.py
@@ -0,0 +1,5 @@
+#s24026 for for
+#for文字列
+
+for i in range(30):
+    print("s24"+f"{i+1:03}")
--- if_kadai.py
+++ if_kadai.py
@@ -1,12 +1,12 @@
 #s24026
 #if文条件式
 
-score >= 50
-if score >= 60:
-    print("可")
+score = 55
+if score >= 80:
+    print("优")
 elif score >= 70:
     print("良")
-elif score >= 80:
-    print("优")
+elif score >= 60:
+    print("可")
 else:
     print("不可")
