a8fb6ea86d45f64687e4272a0f49e01dd9c0d565
itc-s24026 <s24026@std.it-college.ac.jp>
2024-06-17 06:54:23 +0900

    課題を提出しています

--- def4_kadai.py
+++ def4_kadai.py
@@ -1,10 +1,14 @@
-#entry_rensyu.py
-#main()関数をエントリーポイントとして使用する練習
+
+
+import random
+
+def omikuji():
+    kuji = ["大吉","中吉","小吉","凶"]
+    return random.choice(kuji)
 
 def main():
-    print("Hello World!")
+    kekka = omikuji()
+    print("结果",kekka,"です。")
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
-    
-
