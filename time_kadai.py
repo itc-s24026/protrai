3015ff70b62b6135a8337d170ec89a84e9d4121c
itc-s24026 <167944346+itc-s24026@users.noreply.github.com>
2024-06-24 16:18:41 +0900

    Update time_kadai.py

--- time_kadai.py
+++ time_kadai.py
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-# s24026
-# 制作能显示时间的时间app.
-# 把名字定为[time_kadai.py]
-
-import datetime
-import tkinter as tk # CUI制作可以显示的模块。
-
-# 处理能显示时间的函数
-def update_time():
-    now = datetime.datetime.now()
-    current_time = (now.strftime("%Y年%m月%d日 %H時%M文%S秒"))
-    #
-    lbl.config(text=current_time)
-    root.after(1000, update_time) #自己也可以被叫出的意思
-
-# 时间app制作
-root = tk.Tk()
-
-root.title("時計アプリ")
-
-#调整始终软件的大小
-lbl = tk.Label()
-lbl.config(text="",font=("Helvetica",20))
-lbl.pack()
-
-#叫出函数
-update_time()
-
-root.mainloop()
-=======
 #import datetime
 #print(datetime.datetime.now()) 用导入模块的方式查看当前系统时间
 
